8368605a493df2b8152d36948d181a367f945e1f
Lordaxelfromtb69 <axelbe@live.dk>
2023-06-15 19:58:59 +0200

    virker nu!

--- Prøveeksamen/Program.cs
+++ Prøveeksamen/Program.cs
@@ -5,8 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerApi.Controllers
 {
-    [ApiController] // Angiver, at denne controller er en API-controller
-    [Route("api/[controller]")] // Angiver rutepræfikset for alle handlinger i denne controller. [controller] erstattes med controllerens navn ("Customers").
+    [ApiController]
+    [Route("api/[controller]")]
     public class CustomersController : ControllerBase
     {
         // Simuleret liste over kunder (erstattes normalt af en database)
@@ -21,41 +21,40 @@ namespace CustomerApi.Controllers
         [HttpGet]
         public IActionResult GetCustomers()
         {
-            return Ok(customers); // Returnerer alle kunder
+            return Ok(customers);
         }
 
         // GET /api/customers/{id}
         [HttpGet("{id}")]
         public IActionResult GetCustomer(int id)
         {
-            var customer = customers.FirstOrDefault(c => c.Id == id); // Finder kunden med den angivne id
+            var customer = customers.FirstOrDefault(c => c.Id == id);
             if (customer == null)
-                return NotFound(); // Hvis kunden ikke findes, returneres en NotFound-fejl
+                return NotFound();
 
-            return Ok(customer); // Returnerer den fundne kunde
+            return Ok(customer);
         }
 
         // POST /api/customers
         [HttpPost]
         public IActionResult PostCustomer([FromBody] Customer customer)
         {
-            customer.Id = customers.Max(c => c.Id) + 1; // Genererer en ny id til den nye kunde
-            customers.Add(customer); // Tilføjer den nye kunde til listen
+            customer.Id = customers.Max(c => c.Id) + 1;
+            customers.Add(customer);
 
             return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
-            // Returnerer en Created-respons med en URL til den nye kunde og kunden selv
         }
 
         // DELETE /api/customers/{id}
         [HttpDelete("{id}")]
         public IActionResult DeleteCustomer(int id)
         {
-            var customer = customers.FirstOrDefault(c => c.Id == id); // Finder kunden med den angivne id
+            var customer = customers.FirstOrDefault(c => c.Id == id);
             if (customer == null)
-                return NotFound(); // Hvis kunden ikke findes, returneres en NotFound-fejl
+                return NotFound();
 
-            customers.Remove(customer); // Fjerner kunden fra listen
-            return NoContent(); // Returnerer en NoContent-respons for at indikere, at handlingen er lykkedes
+            customers.Remove(customer);
+            return NoContent();
         }
 
         // GET /api/emails/{type}
@@ -64,12 +63,27 @@ namespace CustomerApi.Controllers
         {
             var emails = customers.Where(c => c.Type.Equals(type, StringComparison.OrdinalIgnoreCase))
                                  .Select(c => c.Email)
-                                 .ToList(); // Filtrer og udvælg e-mailadresser baseret på den angivne type
+                                 .ToList();
 
-            return Ok(emails); // Returnerer listen over e-mailadresser
+            return Ok(emails);
+        }
+
+        public static void Main(string[] args)
+        {
+            // Opret og konfigurer en webhost
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            var app = builder.Build();
+
+            // Tilføj controllers til appen
+            app.MapControllers();
+
+            // Start appen
+            app.Run();
         }
     }
 
+    // Modelklasse for kunder
     public class Customer
     {
         public int Id { get; set; }
