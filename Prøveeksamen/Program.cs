e76bd8f73b21b2f15f675111f941cccaefb4de8c
Lordaxelfromtb69 <axelbe@live.dk>
2023-06-15 19:47:30 +0200

    virker

--- Prøveeksamen/Program.cs
+++ Prøveeksamen/Program.cs
@@ -1,74 +1,80 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CustomerApi.Controllers
 {
+    [ApiController] // Angiver, at denne controller er en API-controller
+    [Route("api/[controller]")] // Angiver rutepræfikset for alle handlinger i denne controller. [controller] erstattes med controllerens navn ("Customers").
+    public class CustomersController : ControllerBase
+    {
+        // Simuleret liste over kunder (erstattes normalt af en database)
+        private static List<Customer> customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "John Doe", Email = "john@example.com", Type = "privat" },
+            new Customer { Id = 2, Name = "Jane Smith", Email = "jane@example.com", Type = "erhverv" },
+            new Customer { Id = 3, Name = "Bob Johnson", Email = "bob@example.com", Type = "privat" }
+        };
+
+        // GET /api/customers
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(customers); // Returnerer alle kunder
+        }
+
+        // GET /api/customers/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == id); // Finder kunden med den angivne id
+            if (customer == null)
+                return NotFound(); // Hvis kunden ikke findes, returneres en NotFound-fejl
+
+            return Ok(customer); // Returnerer den fundne kunde
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public IActionResult PostCustomer([FromBody] Customer customer)
+        {
+            customer.Id = customers.Max(c => c.Id) + 1; // Genererer en ny id til den nye kunde
+            customers.Add(customer); // Tilføjer den nye kunde til listen
+
+            return CreatedAtAction(nameof(GetCustomer), new { id = customer.Id }, customer);
+            // Returnerer en Created-respons med en URL til den nye kunde og kunden selv
+        }
+
+        // DELETE /api/customers/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == id); // Finder kunden med den angivne id
+            if (customer == null)
+                return NotFound(); // Hvis kunden ikke findes, returneres en NotFound-fejl
+
+            customers.Remove(customer); // Fjerner kunden fra listen
+            return NoContent(); // Returnerer en NoContent-respons for at indikere, at handlingen er lykkedes
+        }
+
+        // GET /api/emails/{type}
+        [HttpGet("emails/{type}")]
+        public IActionResult GetEmailsByType(string type)
+        {
+            var emails = customers.Where(c => c.Type.Equals(type, StringComparison.OrdinalIgnoreCase))
+                                 .Select(c => c.Email)
+                                 .ToList(); // Filtrer og udvælg e-mailadresser baseret på den angivne type
+
+            return Ok(emails); // Returnerer listen over e-mailadresser
+        }
+    }
+
     public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Type { get; set; }
-    }
-
-    public class CustomersController : ApiController
-    {
-        private static List<Customer> customers = new List<Customer>
-        {
-            new Customer { Id = 1, Name = "Magnus cocksucker", Email = "suck@cock.com", Type = "privat" },
-            new Customer { Id = 2, Name = "fed benny", Email = "fed@tyksak.com", Type = "erhverv" },
-            new Customer { Id = 3, Name = "Bob Johnson", Email = "bob@asshole.com", Type = "privat" }
-        };
-
-        // GET /api/kunder
-        public IHttpActionResult GetCustomers()
-        {
-            return Ok(customers);
-        }
-
-        // GET /api/kunder/{id}
-        public IHttpActionResult GetCustomer(int id)
-        {
-            var customer = customers.FirstOrDefault(c => c.Id == id);
-            if (customer == null)
-                return NotFound();
-
-            return Ok(customer);
-        }
-
-        // POST /api/kunder
-        public IHttpActionResult PostCustomer(Customer customer)
-        {
-            customer.Id = customers.Max(c => c.Id) + 1;
-            customers.Add(customer);
-
-            return CreatedAtRoute("DefaultApi", new { id = customer.Id }, customer);
-        }
-
-        // DELETE /api/kunder/{id}
-        public IHttpActionResult DeleteCustomer(int id)
-        {
-            var customer = customers.FirstOrDefault(c => c.Id == id);
-            if (customer == null)
-                return NotFound();
-
-            customers.Remove(customer);
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // GET /api/emails/{type}
-        [Route("api/emails/{type}")]
-        public IHttpActionResult GetEmailsByType(string type)
-        {
-            var emails = customers.Where(c => c.Type.Equals(type, StringComparison.OrdinalIgnoreCase))
-                                 .Select(c => c.Email)
-                                 .ToList();
-
-            return Ok(emails);
-        }
     }
 }
