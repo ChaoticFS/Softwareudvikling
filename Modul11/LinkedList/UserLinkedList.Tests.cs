edcfbdd4d1796d937b29a606bea9cba8594840a6
Lordaxelfromtb69 <axelbe@live.dk>
2023-06-17 19:20:58 +0200

    kommentar

--- Modul11/LinkedList/UserLinkedList.Tests.cs
+++ Modul11/LinkedList/UserLinkedList.Tests.cs
@@ -8,61 +8,86 @@ namespace LinkedList.Tests
         [TestMethod]
         public void TestAddFirst()
         {
+            // Opretter brugerobjekter
             User kristian = new User("Kristian", 1);
             User mads = new User("Mads", 2);
             User torill = new User("Torill", 3);
 
+            // Opretter en brugerlinket liste
             UserLinkedList list = new UserLinkedList();
+
+            // Tilføjer brugerobjektet som den første i listen
             list.AddFirst(kristian);
+
+            // Tester om den første bruger i listen er den samme som den tilføjede bruger
             Assert.AreEqual(kristian, list.GetFirst());
         }
 
         [TestMethod]
         public void TestRemoveFirst()
         {
+            // Opretter brugerobjekter
             User kristian = new User("Kristian", 1);
             User mads = new User("Mads", 2);
             User torill = new User("Torill", 3);
 
+            // Opretter en brugerlinket liste
             UserLinkedList list = new UserLinkedList();
+
+            // Tilføjer brugerobjekterne til listen som de første elementer
             list.AddFirst(kristian);
             list.AddFirst(mads);
             list.AddFirst(torill);
+
+            // Fjerner den første bruger fra listen og tester om den fjernede bruger er den forventede
             Assert.AreEqual(torill, list.RemoveFirst());
         }
 
         [TestMethod]
         public void TestCountUsers()
         {
+            // Opretter brugerobjekter
             User kristian = new User("Kristian", 1);
             User mads = new User("Mads", 2);
             User torill = new User("Torill", 3);
 
+            // Opretter en brugerlinket liste
             UserLinkedList list = new UserLinkedList();
+
+            // Tilføjer brugerobjekterne til listen som de første elementer
             list.AddFirst(kristian);
             list.AddFirst(mads);
             list.AddFirst(torill);
+
+            // Tester om antallet af brugere i listen er det forventede antal
             Assert.AreEqual(3, list.CountUsers());
         }
 
         [TestMethod]
         public void TestRemoveUser()
         {
+            // Opretter brugerobjekter
             User kristian = new User("Kristian", 1);
             User mads = new User("Mads", 2);
             User torill = new User("Torill", 3);
             User henrik = new User("Henrik", 5);
             User klaus = new User("Klaus", 6);
 
+            // Opretter en brugerlinket liste
             UserLinkedList list = new UserLinkedList();
+
+            // Tilføjer brugerobjekterne til listen som de første elementer
             list.AddFirst(kristian);
             list.AddFirst(mads);
             list.AddFirst(torill);
             list.AddFirst(henrik);
             list.AddFirst(klaus);
 
+            // Fjerner en bruger fra listen og tester om antallet af brugere er det forventede antal
             list.RemoveUser(mads);
             Assert.AreEqual(4, list.CountUsers());
+
+            // Fjerner en anden bruger fra listen og tester igen om antallet af brugere er det forventede antal
             list.RemoveUser(kristian);
             Assert.AreEqual(3, list.CountUsers());
         }
@@ -70,21 +95,25 @@ namespace LinkedList.Tests
         [TestMethod]
         public void TestGetLast()
         {
+            // Opretter brugerobjekter
             User kristian = new User("Kristian", 1);
             User mads = new User("Mads", 2);
             User torill = new User("Torill", 3);
             User henrik = new User("Henrik", 5);
             User klaus = new User("Klaus", 6);
 
+            // Opretter en brugerlinket liste
             UserLinkedList list = new UserLinkedList();
+
+            // Tilføjer brugerobjekterne til listen som de første elementer
             list.AddFirst(kristian);
             list.AddFirst(mads);
             list.AddFirst(torill);
             list.AddFirst(henrik);
             list.AddFirst(klaus);
 
+            // Tester om den sidste bruger i listen er den forventede bruger
             Assert.AreEqual(kristian.Name, list.GetLast().Name);
         }
-
     }
 }
